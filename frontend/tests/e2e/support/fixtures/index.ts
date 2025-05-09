250edb2d545ab8c4f7bb4683ed772fa8f9ce3a87
Manoel Lopes <manoelvitorka@gmail.com>
2025-05-09 12:00:39 +0100

    Rwa 2 (#12)
    * add datacontex / page files / tests

--- frontend/tests/e2e/support/fixtures/index.ts
+++ frontend/tests/e2e/support/fixtures/index.ts
@@ -1,6 +1,8 @@
 import { Page, test as base, expect } from '@playwright/test';
 
+import { ArticleEditorPage } from '../pages/article-editor-page';
 import { FeedPage } from '../pages/feed-page';
+import { MyProfilePage } from '../pages/my-profile-page';
 import { PageFixtures } from '../types';
 import { SignInPage } from '../pages/signin-page';
 import { SignUpPage } from '../pages/signup-page';
@@ -12,6 +14,8 @@ const test = base.extend<PageFixtures>({
     context.signUp = new SignUpPage(page);
     context.signIn = new SignInPage(page);
     context.feedPage = new FeedPage(page);
+    context.articleEditor = new ArticleEditorPage(page);
+    context.myProfile = new MyProfilePage(page);
 
     await use(context);
   },
---
+++ frontend/tests/e2e/write-article.spec.ts
@@ -0,0 +1,36 @@
+import { UserCredentials } from './support/types';
+import { test } from './support/fixtures';
+
+test.describe('Write Article', () => {
+  test('Logged-in user creates an article (title, body, tags) and Article appears in “My Articles” list', async ({
+    page,
+  }) => {
+    /**
+     * As a logged-in user, I want to be able to create an article with a title, body, and tags so that I can share my thoughts with the community.
+     * And I want to see the article in my articles list after publishing it.     *
+     */
+    if (!process.env['USER_EMAIL_1'] || !process.env['USER_PASSWORD_1']) {
+      throw new Error(
+        'User credentials are not set in the environment variables, they must be set on the .env file'
+      );
+    }
+    const userCredentials: UserCredentials = {
+      username: process.env['USER_USERNAME_1'],
+      email: process.env['USER_EMAIL_1'],
+      password: process.env['USER_PASSWORD_1'],
+    };
+
+    await page.signIn.navigateToSignInPage();
+    await page.signIn.performSignIn(userCredentials);
+
+    await page.feedPage.validateSignIn();
+
+    await page.articleEditor.navigateToNewArticlePage();
+    await page.articleEditor.validateNewArticlePage();
+    await page.articleEditor.createNewArticle();
+
+    await page.myProfile.navigateToMyProfilePage(userCredentials);
+    await page.myProfile.validateProfilePage(userCredentials);
+    await page.myProfile.validatePublishedArticle();
+  });
+});
